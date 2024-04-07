3406e16c0c8a407dbd66156f1446137386956c4d
SATOSHI NAKAMOTO <satoshinakamoto@SATOSHIs-MBP.cablelynx.com>
2024-04-07 16:51:27 -0500

    Correcting flask server issue

--- main.py
+++ main.py
@@ -67,3 +67,8 @@ def get_moods():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
+
+if __name__ == '__main__':
+    init_db()
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment, default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)  # Bind to all interfaces
